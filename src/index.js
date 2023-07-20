29dac8583573a6ed7b32d2ff5b3f5efdc8195dc1
offiongbassey <naijaspeed1@gmail.com>
2023-07-19 20:21:36 -0700

    cors updated and yarn.error.log added to gitignore

--- src/index.js
+++ src/index.js
@@ -10,10 +10,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.urlencoded({extended: true}));
 
-app.use(cors({
-    origin: ["http://localhost:3000"],
-    credentials: true
-}));
+app.use(cors("*"));
 
 app.get('/', (req, res) => {
     res.send("Backend Connected");
